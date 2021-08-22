17fd6d92ed50d92f340874ccdfbe473570c204fb
Cryptweb <58373834+Cryptweb@users.noreply.github.com>
2021-08-22 16:44:53 +0300

    Update uilibtest.lua

--- uilibtest.lua
+++ uilibtest.lua
@@ -1,16 +1,20 @@
-local Library = {}
+local lib = {}
 
-local LibName = tostring(math.random(1, 100))..tostring(math.random(1,50))..tostring(math.random(1, 100))
+for _, gui in pairs(game:GetService("CoreGui"):GetChildren()) do
+   if gui:IsA("ScreenGui") and string.match(gui.Name, "_SovwUILibrary") then
+      gui:Destroy() 
+   end
+end
+
+local LibName = tostring(math.random(1, 100))..tostring(math.random(1,50))..tostring(math.random(1, 100).."_SovwUILibrary")
 local TweenService = game:GetService("TweenService")
 
-function Library:UI()
+function lib:UI()
     game.CoreGui[LibName].Enabled = not game.CoreGui[LibName].Enabled
 end
 
-function Library:CreateWindow(title)
-    title = title or "Library"
-    
-    local UILIB = Instance.new("ScreenGui")
+function lib:Window(title)
+    local Test = Instance.new("ScreenGui")
     local Dragify = Instance.new("Frame")
     local Main = Instance.new("Frame")
     local TopBar = Instance.new("Frame")
@@ -18,13 +22,15 @@ function Library:CreateWindow(title)
     local Arrow = Instance.new("TextButton")
     local SecondMain = Instance.new("Frame")
     local MainScroll = Instance.new("ScrollingFrame")
+    
     local ContainerLayout = Instance.new("UIListLayout")
 
-    UILIB.Name = LibName
-    UILIB.Parent = game.CoreGui
-    UILIB.ResetOnSpawn = false
-	
-	function dragify(Frame)
+    Test.Name = LibName
+    Test.Parent = game.CoreGui
+    Test.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    Test.ResetOnSpawn = false
+    
+    function dragify(Frame)
 		dragToggle = nil
 		dragSpeed = .01
 		dragInput = nil
@@ -64,9 +70,9 @@ function Library:CreateWindow(title)
 	end
 
     Dragify.Name = "Dragify"
-    Dragify.Parent = UILIB
+    Dragify.Parent = Test
     Dragify.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Dragify.BackgroundTransparency = 1
+    Dragify.BackgroundTransparency = 1.000
     Dragify.BorderSizePixel = 0
     Dragify.Position = UDim2.new(0.0148975793, 0, 0.0276422761, 0)
     Dragify.Size = UDim2.new(0, 181, 0, 442)
@@ -77,7 +83,6 @@ function Library:CreateWindow(title)
     Main.Parent = Dragify
     Main.BackgroundColor3 = Color3.fromRGB(10, 10, 10)
     Main.Size = UDim2.new(0, 181, 0, 26)
-    Main.BorderSizePixel = 0
 
     TopBar.Name = "TopBar"
     TopBar.Parent = Main
@@ -98,6 +103,7 @@ function Library:CreateWindow(title)
     Title.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
     Title.TextXAlignment = Enum.TextXAlignment.Left
 
+    Arrow.Name = "Arrow"
     Arrow.Parent = TopBar
     Arrow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Arrow.BackgroundTransparency = 1.000
@@ -116,53 +122,244 @@ function Library:CreateWindow(title)
     SecondMain.BorderSizePixel = 0
     SecondMain.Position = UDim2.new(0, 0, 0.991710961, 0)
     SecondMain.Size = UDim2.new(0, 181, 0, 233)
+    SecondMain.ClipsDescendants = true
 
     MainScroll.Name = "MainScroll"
     MainScroll.Parent = SecondMain
     MainScroll.Active = true
     MainScroll.BackgroundColor3 = Color3.fromRGB(10, 10, 10)
     MainScroll.BorderSizePixel = 0
-    MainScroll.Position = UDim2.new(0, 0, 0, 0)
+    MainScroll.Position = UDim2.new(0.0525027104, 0, 0.0414285958, 0)
     MainScroll.Size = UDim2.new(0, 164, 0, 211)
     MainScroll.CanvasSize = UDim2.new(0, 0, 0, 0)
+    MainScroll.ClipsDescendants = true
+    MainScroll.ScrollBarThickness = 4
+    MainScroll.ScrollBarImageColor3 = Color3.fromRGB(42, 44, 50)
 
     ContainerLayout.Name = "ContainerLayout"
     ContainerLayout.Parent = MainScroll
-    ContainerLayout.Padding = UDim2.new(0, 8)
     ContainerLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    ContainerLayout.Padding = UDim.new(0, 6)
 
-    local Elements = {}
+    MainScroll.ChildAdded:Connect(function()
+		local ab = ContainerLayout.AbsoluteContentSize
+		MainScroll.CanvasSize = UDim2.new(0,0,0,ab.Y)
+	end)
 
-    function Elements:CreateButton(text, callback)
-        text = text or "New Button"
+    local InnerLib = {}
+
+    function InnerLib:Button(text, callback)
+        text = text or "Button"
         callback = callback or function() end
 
-        local TextButton = Instance.new("TextButton")
-        local TextButtonCorner = Instance.new("UICorner")
+        local Button = Instance.new("TextButton")
+        local buttonCorner = Instance.new("UICorner")
 
-        TextButton.Parent = MainScroll
-        TextButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-        TextButton.BorderSizePixel = 0
-        TextButton.Size = UDim2.new(0, 164, 0, 31)
-        TextButton.Font = Enum.Font.GothamSemibold
-        TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-        TextButton.TextSize = 14.000
-        TextButton.Text = text
-        TextButton.MouseButton1Click:Connect(function()
+        Button.Name = "Button"
+        Button.Parent = MainScroll
+        Button.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+        Button.BorderSizePixel = 0
+        Button.Size = UDim2.new(0, 164, 0, 31)
+        Button.Font = Enum.Font.GothamSemibold
+        Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+        Button.TextSize = 14.000
+        Button.TextWrapped = true
+        Button.Text = text
+        Button.MouseButton1Click:Connect(function()
             callback()
 
-            TextButton.TextSize = 0
-            TweenService:Create(TextButton, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 17}):Play()
+            Button.TextSize = 0
+            TweenService:Create(Button, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 17}):Play()
             wait(.2)
-            TweenService:Create(TextButton, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 14}):Play()
+            TweenService:Create(Button, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 14}):Play()
         end)
-        
-        TextButtonCorner.CornerRadius = UDim.new(0, 4)
-        TextButtonCorner.Name = "TextButtonCorner"
-        TextButtonCorner.Parent = TextButton
+
+        buttonCorner.CornerRadius = UDim.new(0, 4)
+        buttonCorner.Name = "buttonCorner"
+        buttonCorner.Parent = Button
     end
 
-    return Elements
+    function InnerLib:Toggle(text, callback)
+        text = text or "Toggle"
+        callback = callback or function() end
+
+        local Toggle = Instance.new("TextButton")
+        local toggleCorner = Instance.new("UICorner")
+
+        Toggle.Name = "Toggle"
+        Toggle.Parent = MainScroll
+        Toggle.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+        Toggle.BorderSizePixel = 0
+        Toggle.Size = UDim2.new(0, 164, 0, 31)
+        Toggle.Font = Enum.Font.GothamSemibold
+        Toggle.Text = text
+        Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
+        Toggle.TextSize = 14.000
+        Toggle.TextWrapped = true
+
+        toggleCorner.CornerRadius = UDim.new(0, 4)
+        toggleCorner.Name = "toggleCorner"
+        toggleCorner.Parent = Toggle
+
+        local togglexd = false
+        
+        Toggle.MouseButton1Click:Connect(function()
+            togglexd = not togglexd
+            callback(togglexd)
+
+            if togglexd == true then
+                Toggle.TextSize = 0
+                TweenService:Create(Toggle, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 17}):Play()
+                TweenService:Create(Toggle, TweenInfo.new(.2, Enum.EasingStyle.Sine), {BackgroundColor3 = Color3.fromRGB(26, 179, 0)}):Play()
+                wait(.2)
+                TweenService:Create(Toggle, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 14}):Play()
+            else
+                Toggle.TextSize = 0
+                TweenService:Create(Toggle, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 17}):Play()
+                TweenService:Create(Toggle, TweenInfo.new(.2, Enum.EasingStyle.Sine), {BackgroundColor3 = Color3.fromRGB(50, 50, 50)}):Play()
+                wait(.2)
+                TweenService:Create(Toggle, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 14}):Play()
+            end
+        end)
+    end
+
+    local dropdownSizes = {}
+    function InnerLib:Dropdown(text, list, callback)
+        text = text or "New Dropdown"
+        list = list or {}
+        callback = callback or function() end
+
+        local DropYSize = 0
+
+        local Dropdown = Instance.new("Frame")
+        local dropdownCorner = Instance.new("UICorner")
+        local dropdownLabel = Instance.new("TextLabel")
+        local dropdownButton = Instance.new("TextButton")
+        local dropdownItems = Instance.new("Frame")
+        local dropdownItemsCorner = Instance.new("UICorner")
+        local UIListLayout = Instance.new("UIListLayout")
+
+        Dropdown.LayoutOrder = 1
+        Dropdown.Name = "Dropdown"
+        Dropdown.Parent = MainScroll
+        Dropdown.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+        Dropdown.Position = UDim2.new(0, 0, 0.369668245, 0)
+        Dropdown.Size = UDim2.new(0, 164, 0, 31)
+
+        dropdownCorner.CornerRadius = UDim.new(0, 4)
+        dropdownCorner.Name = "dropdownCorner"
+        dropdownCorner.Parent = Dropdown
+
+        dropdownLabel.Name = "dropdownLabel"
+        dropdownLabel.Parent = Dropdown
+        dropdownLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+        dropdownLabel.BackgroundTransparency = 1.000
+        dropdownLabel.BorderSizePixel = 0
+        dropdownLabel.Size = UDim2.new(0, 132, 0, 31)
+        dropdownLabel.Font = Enum.Font.Gotham
+        dropdownLabel.Text = "  " .. text
+        dropdownLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+        dropdownLabel.TextSize = 14.000
+        dropdownLabel.TextWrapped = true
+        dropdownLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+        dropdownButton.Name = "dropdownButton"
+        dropdownButton.Parent = Dropdown
+        dropdownButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+        dropdownButton.BackgroundTransparency = 1.000
+        dropdownButton.BorderSizePixel = 0
+        dropdownButton.Position = UDim2.new(0.810975492, 0, 0, 0)
+        dropdownButton.Size = UDim2.new(0, 31, 0, 31)
+        dropdownButton.Font = Enum.Font.Gotham
+        dropdownButton.Text = "+"
+        dropdownButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+        dropdownButton.TextSize = 18.000
+        dropdownButton.TextWrapped = true
+
+        table.insert(dropdownSizes, #dropdownSizes + 1, dropdownItems.Size)
+        dropdownItems.LayoutOrder = 1
+        dropdownItems.Name = #dropdownSizes
+        dropdownItems.Parent = Dropdown
+        dropdownItems.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+        dropdownItems.BorderSizePixel = 0
+        dropdownItems.Position = UDim2.new(0, 0, 0.850000203, 0)
+        dropdownItems.Size = UDim2.new(0, 164, 0, DropYSize)
+        dropdownItems.ClipsDescendants = true
+
+        dropdownItemsCorner.CornerRadius = UDim.new(0, 4)
+        dropdownItemsCorner.Name = "dropdownItemsCorner"
+        dropdownItemsCorner.Parent = dropdownItems
+
+        UIListLayout.Parent = dropdownItems
+        UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+        local tI = TweenInfo.new(.2, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
+        local savedOffset;
+        local tween;
+        local tween2;
+        local tween3;
+
+        local isDropped = false
+        local debounce = false
+
+        dropdownButton.MouseButton1Click:Connect(function()
+            if debounce == false then
+                debounce = true
+                if open == true then
+                    savedOffset = dropdownSizes[tonumber(dropdownItems.Name)] or dropdownItems.Size
+                    tween = TweenService:Create(dropdownItems, tI, {Size = dropdownItems.Size - UDim2.new(0, 0, 0, DropYSize)})
+                    tween:Play()
+
+                    tween2 = TweenService:Create(MainScroll, tI, {Size = UDim2.new(0, 164, 0, MainScroll.Size.Y.Offset - savedOffset.Y.Offset)})
+                    tween2:Play()
+
+                    dropdownButton.Text = "+"
+                    open = false
+                else
+                    savedOffset = dropdownSizes[tonumber(dropdownItems.Name)] or dropdownItems.Size
+                    tween = TweenService:Create(dropdownItems, tI, {Size = dropdownItems.Size + UDim2.new(0, 0, 0, DropYSize)})
+                    tween:Play()
+
+                    tween2 = TweenService:Create(MainScroll, tI, {Size = UDim2.new(0, 164, 0, MainScroll.Size.Y.Offset + savedOffset.Y.Offset)})
+                    tween2:Play()
+
+                    dropdownButton.Text = "-"
+                    open = true
+                end
+
+                wait(0.2)
+                debounce = false
+            end
+        end)
+
+        for i,v in next, list do
+            DropYSize = DropYSize + 31
+
+            local dropdownItemButton = Instance.new("TextButton")
+
+            dropdownItemButton.Name = "dropdownItemButton"
+            dropdownItemButton.Parent = dropdownItems
+            dropdownItemButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            dropdownItemButton.BackgroundTransparency = 1.000
+            dropdownItemButton.BorderSizePixel = 0
+            dropdownItemButton.Size = UDim2.new(0, 164, 0, 31)
+            dropdownItemButton.Font = Enum.Font.Gotham
+            dropdownItemButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+            dropdownItemButton.TextSize = 14.000
+            dropdownItemButton.Text = v
+
+            dropdownItemButton.MouseButton1Click:Connect(function()
+                callback(v)
+                dropdownItemButton.TextSize = 0
+                dropdownLabel.Text = "  "..text..": "..v
+                TweenService:Create(dropdownItemButton, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 17}):Play()
+                wait(.2)
+                TweenService:Create(dropdownItemButton, TweenInfo.new(.2, Enum.EasingStyle.Quad), {TextSize = 14}):Play()
+            end)
+        end
+    end
+
+    return InnerLib
 end
 
-return Library
+return lib
